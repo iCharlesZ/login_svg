967ee7e6a589842330c013f1a920d8b68c027427
Charles zhang <zhangchixx8@gmail.com>
2020-07-10 17:49:09 +0800

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <div id="app">
+    <Login />
+  </div>
+</template>
+
+<script>
+import Login from "./components/Login.vue";
+
+export default {
+  name: "App",
+  components: {
+    Login
+  }
+};
+</script>
+
+<style>
+* {
+  margin: 0;
+  padding: 0;
+}
+
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+}
+</style>
---
+++ src/components/Login.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="login">
+    <div class="svgContainer">
+      <Icon/>
+    </div>
+  </div>
+</template>
+
+<script>
+import Icon from './Icon.vue'
+export default {
+  components: {
+    Icon
+  },
+  name: 'Login'
+}
+</script>
+
+<style scoped>
+
+</style>
